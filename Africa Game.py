a0797bcb2fd5c7f1b5ddb5e6f84b30299bb0d344
Loki-V <144679653+Loki-V@users.noreply.github.com>
2023-09-27 13:06:54 +0100

    Africa Game.py

--- Africa Game.py
+++ Africa Game.py
@@ -4,13 +4,14 @@ print('----------------------Africa Game----------------------\n\n\n\n')
 import time
 score=0
 lives=3
-countries = ["Algeria", "Angola", "Benin", 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad', 'Comoros', 'Ivory Coast', 'Djibouti', 'Democratic Republic of the Congo', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Republic of the Congo', 'Rwanda', 'Sao Tome & Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe' ]
+countries = ["Algeria", "Angola", "Benin", 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad', 'Comoros', 'Ivory Coast', 'Djibouti', 'Democratic Republic Of The Congo', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Republic Of The Congo', 'Rwanda', 'Sao Tome And Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe' ]
 print('---Countries of Africa---\n\n')
 print(f'Score={score}')
 
 while len(countries) >0 and lives >0:
     print(f'Number of countries to guess:{len(countries)}\nScore={score}\nLives Left:{lives}\n\n')
-    country=input('Enter in the name of an African country you have not named already.\n')
+    country=str(input('Enter in the name of an African country you have not named already.\n'))
+    country=country.title()
     if country in countries:
         time.sleep(.5)
         print('Good Guess\n')
@@ -22,7 +23,7 @@ while len(countries) >0 and lives >0:
         print('Invalid Guess\n')
         time.sleep(2)
         lives-=1
-if lives==-1:
-    print('Game Over')
+if lives==0:
+    print(f'Game Over\n\nYou missed these countries:',*countries, sep='\n')
 else:
     print('\nWell Done! You have Completed the Game!')
