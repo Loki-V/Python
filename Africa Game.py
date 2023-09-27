7c4fb4782de615c61e4fb7af3abdbe4f9df7af65
Loki-V <144679653+Loki-V@users.noreply.github.com>
2023-09-27 12:45:33 +0100

    Africa Game.py

--- Africa Game.py
+++ Africa Game.py
@@ -3,12 +3,13 @@
 print('----------------------Africa Game----------------------\n\n\n\n')
 import time
 score=0
+lives=3
 countries = ["Algeria", "Angola", "Benin", 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 'Central African Republic', 'Chad', 'Comoros', 'Ivory Coast', 'Djibouti', 'Democratic Republic of the Congo', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Republic of the Congo', 'Rwanda', 'Sao Tome & Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe' ]
 print('---Countries of Africa---\n\n')
 print(f'Score={score}')
 
-while len(countries) >0:
-    print(f'Number of countries to guess:{len(countries)}\nScore={score}\n\n')
+while len(countries) >0 and lives >0:
+    print(f'Number of countries to guess:{len(countries)}\nScore={score}\nLives Left:{lives}\n\n')
     country=input('Enter in the name of an African country you have not named already.\n')
     if country in countries:
         time.sleep(.5)
@@ -20,7 +21,8 @@ while len(countries) >0:
     else:
         print('Invalid Guess\n')
         time.sleep(2)
-Print('\nWell Done! You have Completed the Game!')
-
-
-
+        lives-=1
+if lives==-1:
+    print('Game Over')
+else:
+    print('\nWell Done! You have Completed the Game!')
