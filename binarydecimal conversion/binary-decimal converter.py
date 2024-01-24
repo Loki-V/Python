14cbc10c37155cb1ac8934a2e7ddb7b4a9b1df9d
Loki-V <144679653+Loki-V@users.noreply.github.com>
2024-01-24 12:15:36 +0000

    Added addition of binary numbers

--- binarydecimal conversion/binary-decimal converter.py
+++ binarydecimal conversion/binary-decimal converter.py
@@ -1,7 +1,7 @@
 import library
 
 print("----------Binary/Decimal Converter----------")
-x = input("\n\n1.Decimal to Binary\n2.Binary to decimal\nenter '1' or '2' to select: ")
+x = input("\n\n1.Decimal to Binary\n2.Binary to decimal\n3.Add two Binary numbers.\nenter the number to select: ")
 
 if x == '1':
     bi = int(input("Enter your binary number: "))
@@ -11,3 +11,8 @@ if x == '1':
 if x == '2':
     dec = int(input("Enter your decimal number: "))
     library.DB(dec)
+    
+if x == '3':
+    binary1 = int(input("Enter first binary number."))
+    binary2 = int(input("Enter second binary number."))
+    library.add(binary1,binary2)
