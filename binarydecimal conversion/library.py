910483c0ebae7ac85fc07a4097c8840e300e7977
Loki-V <144679653+Loki-V@users.noreply.github.com>
2024-01-24 12:16:12 +0000

    added addition of binary numbers function

--- binarydecimal conversion/library.py
+++ binarydecimal conversion/library.py
@@ -27,6 +27,59 @@ def DB(decimal):
     ans = ans.replace('[','')
     ans = ans.replace(']','')
     print(f"The decimal number {decimal} is {ans} in binary.")
-        
-            
     
+def add(binary1,binary2):
+       
+    store = 0
+    _ = 0
+    y = 0
+    ans = []
+    n1 = str(binary1)[::-1]
+    n2 = str(binary2)[::-1]
+    num1 = list(n1)
+    num2 = list(n2)
+    num2.append('0')
+    num1.append('0')
+    if len(n1) < len(n2):
+        num1.append('0')
+        num1.append('0')
+    if len(n2) < len(n1):
+        num2.append('0')
+        num2.append('0')
+        
+    num1 = str(num1)
+    num1 = num1.replace(']','')
+    num1 = num1.replace('[','')
+    num1 = num1.replace(' ','')
+    num1 = num1.replace(',','')
+    num1 = num1.replace('"','')
+    num1 = num1.replace("'","")
+
+    num2 = str(num2)
+    num2 = num2.replace(']','')
+    num2 = num2.replace('[','')
+    num2 = num2.replace(',','')
+    num2 = num2.replace(' ','')
+    num2 = num2.replace('"','')
+    num2 = num2.replace("'","")
+
+    while _ <= len(n1) and _ <= len(n2):
+        y = int(num1[_])+int(num2[_])+store
+        store = 0
+        if y == 2:
+            y = 0
+            store = 1
+        elif y == 3:
+            y = 1
+            store = 1
+        ans.insert(0,y)
+        _ += 1
+
+    ans = str(ans)
+    ans = ans.replace(',','')
+    ans = ans.replace(' ','')
+    ans = ans.replace('[','')
+    ans = ans.replace(']','')
+    print(ans) 
+
+
